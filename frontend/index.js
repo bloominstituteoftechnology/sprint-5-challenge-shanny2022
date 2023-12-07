0fe646db6a93ca4aabd98a5436a44c6f41b7d6e7
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-07 05:32:50 -0500

    update

--- frontend/index.js
+++ frontend/index.js
@@ -5,45 +5,28 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
+async function fetchData() {
+  const [learnersResponse, mentorsResponse] = await Promise.all([
+    axios.get('http://localhost:3003/api/learners'),
+    axios.get('http://localhost:3003/api/mentors'),
+  ]);
 
-  // Fetch data from Endpoint A
+  const learners = learnersResponse.data;
+  const mentors = mentorsResponse.data;
 
+  learners.forEach(learner => {
+    learner.mentors = learner.mentors.map(id => mentors.find(mentor => mentor.id === id).name);
+  });
 
-
-  async function renderLearnerCards() {
-    const [learnersResponse, mentorsResponse] = await Promise.all([
-      axios.get('http://localhost:3003/api/learners'),
-      axios.get('http://localhost:3003/api/mentors'),
-    ]);
-
-    const learners = learnersResponse.data;
-    const mentors = mentorsResponse.data;
-
-    learners.forEach(learner => {
-      learner.mentors = learner.mentors.map(id => mentors.find(mentor => mentor.id === id).name);
-    });
-
-    const container = document.querySelector('.cards');
-
-learners.forEach(learner => {
-const card = createCard(learner);
-container.appendChild(card);
-});
-
-// Return the processed data
-return learners;
+  return learners;
 }
 
 function createCard(learner) {
   const card = document.createElement('div');
   card.className = 'card';
-  card.style.border = '1px solid black';
-  card.style.margin = '10px';
-  card.style.padding = '10px';
 
   const name = document.createElement('h2');
-  name.textContent = learner.name;
-  name.style.color = 'blue';
+  name.textContent = learner.fullName;
   card.appendChild(name);
 
   const email = document.createElement('p');
@@ -58,15 +41,31 @@ function createCard(learner) {
   });
   card.appendChild(mentorList);
 
+  card.addEventListener('click', (event) => {
+    // Check the target and take appropriate action
+  });
+
   return card;
-
 }
-  renderLearnerCards();
+
+async function renderCards() {
+  const learners = await fetchData();
+  const container = document.querySelector('.cards');
+
+  learners.forEach(learner => {
+    const card = createCard(learner);
+    container.appendChild(card);
+  });
+}
+
+renderCards();
 
 
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${2023}`
+
+  const footer = document.querySelector('footer');
+  const currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
