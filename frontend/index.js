4121c5e498765b701e42f932a9efb0655132b2fb
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-09 17:49:57 -0500

    update

--- frontend/index.js
+++ frontend/index.js
@@ -20,7 +20,58 @@ async function fetchData() {
 
   return learners;
 }
+function displayCards(learners) {
+  learners.forEach(learner => {
+    const card = createCard(learner);
+    container.appendChild(card);
+  });
+}
+async function init() {
+  const learners = await fetchData();
+  displayCards(learners);
+}
+init();
+}
+async function highlightMentor(learnerId, mentorId) {
+  const learner = learners.find(l => l.id === learnerId);
+  const mentor = learner.mentors.find(m => m.id === mentorId);
 
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    if(card.innerText.includes(learner.fullName)) {
+      const mentorsList = card.querySelector('ul');
+      const mentorItem = mentorsList.querySelector(`li:contains(${mentor})`);
+      mentorItem.classList.add('highlight');
+    }
+  });
+}
+async function unhighlightMentor(learnerId, mentorId) {
+  const learner = learners.find(l => l.id === learnerId);
+  const mentor = learner.mentors.find(m => m.id === mentorId);
+
+async function toggleMentorHighlight(learnerId, mentorId) {
+  const isHighlighted = mentorId.classList.contains('highlight');
+
+  if(isHighlighted) {
+    await unhighlightMentor(learnerId, mentorId);
+  } else {
+    await highlightMentor(learnerId, mentorId);
+  }
+}
+async function toggleHighlight(learnerId, mentorId) {
+  const isHighlighted = await toggleMentorHighlight(learnerId, mentorId);
+
+  const mentorItem = document.querySelector(`li:contains(${mentorId})`);
+  if(isHighlighted) {
+    mentorItem.classList.add('highlight');
+  } else {
+    mentorItem.classList.remove('highlight');
+  }
+}
+async function fetchData() {
+  const response = await axios.get('http://localhost:3003/api/data');
+  return response.data;
+}
 function createCard(learner) {
   const card = document.createElement('div');
   card.className = 'card';
@@ -41,12 +92,22 @@ function createCard(learner) {
   });
   card.appendChild(mentorList);
 
-  card.addEventListener('click', (event) => {
-    // Check the target and take appropriate action
-  });
-
   return card;
 }
+// Add event listener to each card
+card.addEventListener('click', (event) => {
+  // Check if target is a mentor item
+  if(event.target.tagName === 'LI') {
+    // Get learner and mentor ids
+    const learnerId = learner.id;
+    const mentorId = event.target.textContent;
+
+    // Call toggle highlight function
+    toggleHighlight(learnerId, mentorId);
+  }
+});
+
+
 
 async function renderCards() {
   const learners = await fetchData();
